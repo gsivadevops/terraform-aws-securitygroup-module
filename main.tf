fd0170b4ca52e61b3fb8dd84d84bfa35c5d48667
sivakumar <gsivadevops@gmail.com>
2025-08-21 21:48:25 +0530

    outboud rule for security group

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "aws_security_group" "main" {
   description = var.sg_description
   vpc_id      = var.vpc_id # Reference to an existing or newly created VPC
 
+  # outbound rule to connect to internet
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = merge(
     var.sg_tags,
     local.common_tags,
